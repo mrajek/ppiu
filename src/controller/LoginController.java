33083d58851b97c3bb4c22fd6ff5cafeb55e4c7a
rajek <mrajek9@gmail.com>
2019-04-09 09:51:48 +0200

    Update lab3

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -2,10 +2,24 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
+
+    @FXML
+    private Button bt_logIn;
+
+    @FXML
+    private Button bt_singUp;
+
 
     @FXML
     private TextField tf_login;
@@ -15,12 +29,27 @@ public class LoginController {
 
     @FXML
     void logInAction(ActionEvent event) {
-
+        String login;
+        String password;
+        try {
+            login = tf_login.getText();
+            password = pf_password.getText();
+            if (login.equals("") || password.equals("")) {
+                throw new NullPointerException();
+            }
+        } catch (NullPointerException e){
+            System.out.println("blad");
+        }
     }
 
     @FXML
-    void registerAction(ActionEvent event) {
-
+    void registerAction(ActionEvent event) throws IOException {
+        Stage registerStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/view/singUpView.fxml"));
+        registerStage.setTitle("SalvAction");
+        registerStage.setScene(new Scene(root));
+        registerStage.show();
+        Stage primaryStage = (Stage) bt_singUp.getScene().getWindow();
+        primaryStage.close();
     }
-
 }
--- src/controller/SingUpController.java
+++ src/controller/SingUpController.java
@@ -1,10 +1,19 @@
 package controller;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SingUpController {
 
@@ -37,5 +46,27 @@ public class SingUpController {
 
     @FXML
     private RadioButton rb_sexMale;
+
+    @FXML
+    private Button bt_register;
+
+    @FXML
+    private Button bt_back;
+
+    @FXML
+    void BackAction(ActionEvent event) throws IOException {
+        Stage backStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/view/loginView.fxml"));
+        backStage.setTitle("SalvAction");
+        backStage.setScene(new Scene(root));
+        backStage.show();
+        Stage registerStage = (Stage) bt_back.getScene().getWindow();
+        registerStage.close();
+    }
+
+    @FXML
+    void SingUpAction(ActionEvent event) {
+
+    }
 
 }
